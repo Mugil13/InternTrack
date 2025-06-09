2b7947017667475c668676c324f5b1b35dab872b
Mugilkrishna D U <162177736+Mugil13@users.noreply.github.com>
2025-06-09 14:40:33 +0530

    Update firebase-config.js

--- src/frontend/code/firebase-config.js
+++ src/frontend/code/firebase-config.js
@@ -1,18 +1,1 @@
-// firebase-config.js
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDL4PyOQSb6PAGC8cJopghTn2F_Kd-u-C0",
-  authDomain: "ip-assignment-2be8a.firebaseapp.com",
-  projectId: "ip-assignment-2be8a",
-  storageBucket: "ip-assignment-2be8a.appspot.com",
-  messagingSenderId: "268214222899",
-  appId: "1:268214222899:web:b329012a252634db4f6346"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider };
